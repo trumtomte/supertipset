78b91d04872f4bb834ed5adb8cffe2a73eaa5221
trumtomte <sebbebook@gmail.com>
2014-05-18 19:01:16 +0200

    added new api endpoints

--- assets/js/services/api.js
+++ assets/js/services/api.js
@@ -1,8 +1,8 @@
 angular.module( 'supertipset' ).factory( 'api', ['$http', '$cacheFactory', function( $http, $cacheFactory ) {
     // API error handler
-    function e( m, o ) {
-        return function( d ) {
-            console.log( '[API ERROR]', m, o, '[RESPONSE]', d );
+    function e( message, obj ) {
+        return function( response ) {
+            console.log( '[API ERROR]', message, obj, '[RESPONSE]', response );
         }
     }
 
@@ -13,39 +13,54 @@ angular.module( 'supertipset' ).factory( 'api', ['$http', '$cacheFactory', funct
         // Users
         users: {
             findOne: function( id ) {
-                return $http.get( '/api/users/' + id, { cache: true } ).error( e( 'Unable to find user by id' ) );
+                return $http.get( '/api/users/' + id, { cache: true } ).error( e( 'Unable to find user by id', id ) );
             }
         },
         // User groups
         usergroups: {
             find: function( id ) {
-                return $http.get( '/api/usergroups/' + id, { cache: true } ).error( e( 'Unable to find user groups by id' ) );
+                return $http.get( '/api/usergroups/' + id, { cache: true } ).error( e( 'Unable to find user groups by id', id ) );
             }
         },
         // Groups
         groups: {
             findOne: function( id ) {
-                return $http.get( '/api/groups/' + id, { cache: true } ).error( e( 'Unable to find group by id' ) );
+                return $http.get( '/api/groups/' + id, { cache: true } ).error( e( 'Unable to find group by id', id ) );
             }
         },
         // Rounds
         rounds: {
             find: function( id ) {
-                return $http.get( '/api/rounds/' + id, { cache: true } ).error( e( 'Unable to find rounds by id' ) );
+                return $http.get( '/api/rounds/' + id, { cache: true } ).error( e( 'Unable to find rounds by id', id ) );
             }
         },
         // Bets
         bets: {
             find: function( id ) {
-                return $http.get( '/api/bets/' + id, { cache: true } ).error( e( 'Unable to find bets by id' ) );
+                return $http.get( '/api/bets/' + id, { cache: true } ).error( e( 'Unable to find bets by id', id ) );
             },
             create: function( bets ) {
                 cache.remove( '/api/bets/' + bets.user_id );
-                return $http.post( '/api/bets/' + bets.user_id, bets ).error( e( 'Unable to create a new bet' ) );
+                return $http.post( '/api/bets/' + bets.user_id, bets ).error( e( 'Unable to create a new bet', bets ) );
             },
             update: function( bets ) {
                 cache.remove( '/api/bets/' + bets.user_id );
-                return $http.put( '/api/bets/' + bets.user_id, bets ).error( e( 'Unable to update bets by id' ) );
+                return $http.put( '/api/bets/' + bets.user_id, bets ).error( e( 'Unable to update bets by id', bets ) );
+            }
+        },
+        // Special bets
+        specialbets: {
+            update: function( bets ) {
+                return $http.put( '/api/specialbets/' + bets.user_id, bets ).error( e( 'Unable to update special bets by id', bets ) );
+            }
+        },
+        // Teams
+        teams: {
+            all: function() {
+                return $http.get( '/api/teams', { cache: true } ).error( e( 'Unable to fetch all teams' ) );
+            },
+            findOne: function( id ) {
+                return $http.get( '/api/teams/' + id, { cache: true } ).error( e( 'Unable to fetch team by id', id ) );
             }
         }
     };
