59b286ff26e7abbc5f8960722603724cd318a251
trumtomte <sebbebook@gmail.com>
2014-05-18 19:06:26 +0200

    added user object and placeholder methods for groups management

--- assets/js/controllers/group.js
+++ assets/js/controllers/group.js
@@ -1,5 +1,6 @@
 angular.module( 'supertipset.controllers' ).controller( 'GroupCtrl', ['$scope', '$route', function( $scope, $route ) {
     $scope.group = $route.current.locals.group.data.group;
+    $scope.user = $route.current.locals.user.data.user;
 
     console.log( 'GROUP', $scope.group );
 }]);
--- assets/js/controllers/groups.js
+++ assets/js/controllers/groups.js
@@ -1,5 +1,18 @@
 angular.module( 'supertipset.controllers' ).controller( 'GroupsCtrl', ['$scope', '$route', function( $scope, $route ) {
     $scope.groups = $route.current.locals.groups.data.groups;
+    $scope.user = $route.current.locals.user.data.user;
+
+    $scope.leave = function( group ) {
+        console.log( 'leave group', group );
+    };
+
+    $scope.create = function() {
+        console.log( 'skapa grupp' );
+    };
+
+    $scope.join = function() {
+        console.log( 'gå med i grupp' );
+    };
 
     console.log( 'GROUPS', $scope.groups );
 }]);
