d55e442e2fd1f629b77ef9963935e279a50b2160
trumtomte <sebbebook@gmail.com>
2014-05-18 18:58:37 +0200

    refactored and moved routes + database stuff into separate files

--- server.js
+++ server.js
@@ -1,55 +1,54 @@
 // Dependencies
-var express         = require( 'express' ),
-    mysql           = require( 'mysql' ),
-    auth            = require( './utilities/auth' ),
-    middleware      = require( './utilities/middleware' ),
-    routes          = {},
-    app             = express();
+var express     = require( 'express' ),
+    auth        = require( './utilities/auth' ),
+    middleware  = require( './utilities/middleware' ),
+    routes      = {},
+    app         = express(),
+    // Custom router for API endpoints
+    api         = express.Router();
 
 // HTTP port
 var port = process.env.PORT || 3000;
 
-// Create a MySQL connection
-var conn = mysql.createConnection({
-    host: process.env.MYSQL_HOST || 'localhost',
-    port: process.env.MYSQL_PORT || 3306,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASS || '',
-    database: process.env.MYSQL_DB || 'supertipset'
-});
-
-// Connect to the database
-conn.connect(function( err ) {
-    if ( err ) throw err;
-    console.log( '[SERVER]: Database connection established' );
-});
-
 // Middleware configuration
-middleware( app );
+middleware.conf( app );
 
-// API routes
-['users', 'usergroups', 'groups', 'rounds', 'bets', 'login'].map( function( route ) {
-    routes[route] = require( './routes/' + route )( conn );
-});
+[ // API Routes
+    'users',
+    'usergroups',
+    'groups',
+    'rounds',
+    'bets',
+    'specialbets',
+    'teams',
+    'login'
+].map( function( route ) { routes[route] = require( './routes/' + route ); });
 
-// API
-app.get( '/api/users/:id',      routes.users.findOne );
-app.get( '/api/groups/:id',     routes.groups.findOne );
-app.get( '/api/rounds/:id',     routes.rounds.find );
-app.get( '/api/usergroups/:id', routes.usergroups.find );
-app.get( '/api/bets/:id',       routes.bets.find );
-app.put( '/api/bets/:id',       routes.bets.update );
-app.post( '/api/bets/:id',      routes.bets.create );
+// API endpoints
+api.get( '/users/:id',          routes.users.findOne );
+api.get( '/groups/:id',         routes.groups.findOne );
+api.get( '/rounds/:id',         routes.rounds.find );
+api.get( '/usergroups/:id',     routes.usergroups.find );
+api.get( '/bets/:id',           routes.bets.find );
+api.put( '/bets/:id',           routes.bets.update );
+api.post( '/bets/:id',          routes.bets.create );
+api.put( '/specialbets/:id',    routes.specialbets.update );
+api.get( '/teams',              routes.teams.all );
+api.get( '/teams/:id',          routes.teams.findOne );
+// Define API entry endpoint
+app.use( '/api', api );
 
 // Login
 app.get( '/login',  routes.login.form );
-app.post( '/login', routes.login.login );
 app.get( '/logout', routes.login.logout );
+app.post( '/login', routes.login.login );
 
+// Backend
 app.get( '/app', auth.check, function( req, res ) {
     res.render( 'app', { id: req.session.userId, dev: true } );
 });
 
+// Frontend
 app.get( '/', function( req, res ) {
     res.render( 'index' );
 });
@@ -63,8 +62,15 @@ app.use( function( req, res, next ) {
 
 // Error handler
 app.use( function( err, req, res, next ) {
-    res.status( err.status );
-    res.render( 'error', { status: err.status } );
+    console.log( '[ERROR]', err, '[REQUEST]', req);
+
+    if ( req.path.split( '/' )[1] == 'api' ) {
+        res.json( 500, { statusCode: 500, error: 'Internal Server Error' } );
+    } else {
+        err.status = err.status || 500;
+        res.status( err.status );
+        res.render( 'error', { status: err.status } );
+    }
 });
 
 // Start listening for requests
