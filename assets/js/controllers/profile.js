3f101c090a53f7bc2d835375ace55fc19c987478
trumtomte <sebbebook@gmail.com>
2014-05-18 19:07:05 +0200

    added variable to check if the user is viewing another user, +stub function for group management

--- assets/js/controllers/profile.js
+++ assets/js/controllers/profile.js
@@ -4,9 +4,15 @@ angular.module( 'supertipset.controllers' ).controller( 'ProfileCtrl', ['$scope'
 
     if ( $route.current.params.id ) {
         console.log( 'other user' );
+        $scope.user.current = false;
     } else {
         console.log( 'current user' );
+        $scope.user.current = true;
     }
+
+    $scope.leave = function( group ) {
+        console.log( 'leave group', group );
+    };
 
     console.log( 'PROFILE', $scope.groups, $scope.user );
 }]);
