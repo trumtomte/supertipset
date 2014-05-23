2e05de76dd85e8d42c12d2e7c94fe4b16c579031
Sebastian Bengtegård <sebbe@Sebastians-MacBook-Air.local>
2014-05-23 11:21:34 +0200

    added group description editing functionality

--- assets/js/controllers/group.js
+++ assets/js/controllers/group.js
@@ -1,7 +1,28 @@
-angular.module( 'supertipset.controllers' ).controller( 'GroupCtrl', ['$scope', '$route', function( $scope, $route ) {
+angular.module( 'supertipset.controllers' ).controller( 'GroupCtrl', ['$scope', '$route', 'api', 'ngNotify', function( $scope, $route, api, notify ) {
     $scope.group = $route.current.locals.group.data.group;
     $scope.user = $route.current.locals.user.data.user;
+    $scope.isEditing = false;
 
-    console.log( 'GROUP', $scope.group );
+    $scope.edit = function() {
+        $scope.isEditing = ! $scope.isEditing;
+    };
+
+    $scope.save = function() {
+        var success = function( result ) {
+            $scope.isEditing = false;
+            notify( 'main' ).info( 'Gruppen är uppdaterad!' );
+        };
+
+        var params = {
+            id: $scope.group.id,
+            description: $scope.group.description
+        };
+
+        api.groups.update( params ).success( success );
+    };
+
+    $scope.password = function() {
+
+    };
 }]);
 
