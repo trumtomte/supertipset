a2fd3fe8cb70fe222e812c9166b1a2db4d878209
Sebastian Bengtegård <sebbe@Sebastians-MacBook-Air.local>
2014-05-23 09:59:03 +0200

    added function for generating password hash for the database

--- utilities/auth.js
+++ utilities/auth.js
@@ -15,19 +15,28 @@ exports.compare = function( input, password, fn ) {
     var parts = password.split( '::' ),
         salt = parts[0],
         databaseHash = parts[1],
-        iterations = +parts[2];
+        iterations = +parts[2]
+        len = 64;
 
     // Generate hash based on parts from the database and compare it to the submitted hash
-    crypto.pbkdf2( input, salt, iterations, 64, function( err, hash ) {
-        if ( err ) {
-            console.log( err.toString() );
-            return fn( err );
-        }
+    crypto.pbkdf2( input, salt, iterations, len, function( err, hash ) {
+        if ( err ) throw err;
 
         if ( databaseHash != hash.toString( 'base64' ) ) {
             return fn( new Error( 'Database hash does not match input hash' ) );
         }
 
         fn();
+    });
+};
+
+// Generates a hash based on input password
+exports.generate = function( password, fn ) {
+    var salt = crypto.randomBytes( 64 ).toString( 'base64' ),
+        iter = Math.floor( Math.random() * ( 1200 - 1000 + 1 ) ) + 1000,
+        len = 64;
+
+    crypto.pbkdf2( password, salt, iter, len, function( err, hash ) {
+        fn( err, [salt, hash, iter].join( '::' ));
     });
 };
