517cd4b19ef1e73ee807b36c05f5b4cf682b41ac
Sebastian Bengtegård <sebbe@sebastians-air.lan>
2014-06-01 21:58:04 +0200

    check if the href == current route

--- assets/js/controllers/app.js
+++ assets/js/controllers/app.js
@@ -1,5 +1,10 @@
-angular.module( 'supertipset.controllers' ).controller( 'AppCtrl', ['$scope', function( $scope ) {
+angular.module( 'supertipset.controllers' ).controller( 'AppCtrl', ['$scope', '$location', function( $scope, $location ) {
     $scope.$back = function() {
         window.history.back();
+    };
+    $scope.isActive = function( path ) {
+        var re = new RegExp( '^' + path );
+        return re.test( $location.path() );
+        // return path == $location.path();
     };
 }]);
