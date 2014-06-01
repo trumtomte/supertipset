e239739c85267af5e8c8f943db610b29526921b8
Sebastian Bengtegård <sebbe@sebastians-air.lan>
2014-06-01 22:00:44 +0200

    return array without object instead of removing it

--- assets/js/filters/except.js
+++ assets/js/filters/except.js
@@ -1,8 +1,6 @@
 angular.module( 'supertipset' ).filter( 'except', function() {
     // Removes an object from an array with the given id
     return function( array, id ) {
-        _.remove( array, function( i ) { return i.id == id; } );
-        return array;
+        return _.reject( array, function( i ) { return i.id == id; } );
     };
 });
-
