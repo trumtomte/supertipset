a726fa205152655503901bf375981de01eab7946
trumtomte <sebbebook@gmail.com>
2014-07-13 18:56:14 +0200

    refactored for browserify

---
+++ assets/js/directives/index.js
@@ -0,0 +1,6 @@
+// Export all directives
+module.exports = [
+    { name: 'round', value: require( './round' ) },
+    { name: 'game', value: require( './game' ) },
+    { name: 'specialbet', value: require( './specialbet' ) }
+];
--- assets/js/directives/round.js
+++ assets/js/directives/round.js
@@ -1,17 +1,21 @@
-angular.module( 'supertipset' ).directive( 'round', function() {
+function RoundDirective() {
+    function Linker( $scope, $element, $attr ) {
+        var now = new Date(),
+            roundStart = new Date( Date.parse( $scope.round.start ) ),
+            roundStop = new Date( Date.parse( $scope.round.stop ) );
+
+        // If the round is within the date of today it is considered "current/active"
+        $scope.isCurrentRound = roundStart < now && roundStop > now ? true : false;
+    }
+
     var directive = {
         require: '^ngModel',
         transclude: true,
-        templateUrl: '/assets/templates/round.html',
-        link: function( $scope, $element, $attr ) {
-            var now = new Date(),
-                roundStart = new Date( Date.parse( $scope.round.start ) ),
-                roundStop = new Date( Date.parse( $scope.round.stop ) );
-
-            // If the round is within the date of today it is considered "current/active"
-            $scope.isCurrentRound = roundStart < now && roundStop > now ? true : false;
-        }
+        templateUrl: 'round.html',
+        link: Linker
     };
 
     return directive;
-});
+}
+
+module.exports = RoundDirective;
