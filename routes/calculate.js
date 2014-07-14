d5ed76a1358dabb489dab7144f639d6e70f9736a
trumtomte <sebbebook@gmail.com>
2014-07-14 22:09:04 +0200

    route for calculating special bets

--- routes/calculate.js
+++ routes/calculate.js
@@ -2,10 +2,11 @@ var db = require( '../utilities/database' ),
     auth = require( '../utilities/auth' ),
     User = require( '../models/user' ).User,
     Game = require( '../models/game' ).Game;
+    SpecialBetCollection = require( '../models/specialbet' ).SpecialBetCollection;
 
 // Calculate points for each user based on game results and user bets
 // This requires the password of one specific user
-exports.game = function( req, res, next ) {
+exports.game = function( req, res ) {
     var user = new User();
 
     user.findByUsername( 'superuser', function( err, data ) {
@@ -38,6 +39,62 @@ exports.game = function( req, res, next ) {
                         if ( err ) return res.redirect( '/admin-calculate?error=1' );
                         res.redirect( '/admin-calculate?success=1' );
                     });
+                });
+            });
+        });
+    });
+};
+
+exports.special = function( req, res ) {
+    // TODO make dynamic
+    var tournamentID = 1,
+        teamID = 40,
+        playerID = 204,
+        goals = 6;
+
+    var user = new User(),
+        specialBetCollection = new SpecialBetCollection( tournamentID );
+
+    var results = [];
+
+    user.findByUsername( 'superuser', function( err, data ) {
+        if ( err || ! data ) return res.redirect( '/admin-calculate?error=2' );
+        console.log( 'found user' );
+
+        auth.compare( req.body.password, data.password, function( err ) {
+            if ( err ) return res.redirect( '/admin-calculate?error=2' );
+            console.log( 'auth' );
+
+            specialBetCollection.fetch( function( err, bets ) {
+                if ( err || ! bets ) return res.redirect( '/admin-calculate?error=2' );
+                console.log( 'found bets', bets );
+
+                bets.forEach( function( bet ) {
+                    var result = [bet.user_id, 0, 0, 0, tournamentID];
+
+                    // Points from correct goals can only be set
+                    // if the correct player was chosen
+                    if ( bet.player_id == playerID ) {
+                        result[1] = 25;
+
+                        if ( bet.player_goals == goals ) {
+                            result[2] = 25;
+                        }
+                    }
+
+                    if ( bet.team_id == teamID ) {
+                        result[3] = 50;
+                    }
+
+                    results.push( result );
+                });
+
+                console.log( results );
+
+                specialBetCollection.saveResults( [results], function( err ) {
+                    console.log( 'wanted to save results' );
+                    if ( err ) return res.redirect( '/admin-calculate?error=2' );
+                    res.redirect( '/admin-calculate?success=2' );
                 });
             });
         });
