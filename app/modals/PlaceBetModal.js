f133627b2241df8b1251c9d080d72d4769ad6b6f
Sebastian Bengtegård <sebbebook@gmail.com>
2016-06-09 16:21:34 +0200

    added select instead of input fields for betting

--- app/modals/PlaceBetModal.js
+++ app/modals/PlaceBetModal.js
@@ -55,6 +55,20 @@ const PlaceBetModal = ({ user, game, dispatch }) => {
     const setData = e => data[e.target.name] = Number(e.target.value)
     const focus = r => r ? r.focus() : false
 
+    // <input
+    //     onChange={setData}
+    //     ref={focus}
+    //     defaultValue={betExists ? bet.team_1_bet : 0}
+    //     min='0'
+    //     type='number'
+    //     name='teamOne' />
+    // <input
+    //     onChange={setData}
+    //     defaultValue={betExists ? bet.team_2_bet : 0}
+    //     min='0'
+    //     type='number'
+    //     name='teamTwo' />
+
     return (
         <Modal submit={submit}>
             <h2 className='modal-title'>Tippa</h2>
@@ -62,19 +76,42 @@ const PlaceBetModal = ({ user, game, dispatch }) => {
                 {game.team_1.name} -  {game.team_2.name}
             </p>
             <div className='game-bets'>
-                <input
+                <select
                     onChange={setData}
-                    ref={focus}
-                    defaultValue={betExists ? bet.team_1_bet : 0}
-                    min='0'
-                    type='number'
-                    name='teamOne' />
-                <input
+                    defaultValue={betExists ? bet.team_1_bet: 0}
+                    name='teamOne'>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
+                <select
                     onChange={setData}
-                    defaultValue={betExists ? bet.team_2_bet : 0}
-                    min='0'
-                    type='number'
-                    name='teamTwo' />
+                    defaultValue={betExists ? bet.team_2_bet: 0}
+                    name='teamTwo'>
+                    <option value="0">0</option>
+                    <option value="1">1</option>
+                    <option value="2">2</option>
+                    <option value="3">3</option>
+                    <option value="4">4</option>
+                    <option value="5">5</option>
+                    <option value="6">6</option>
+                    <option value="7">7</option>
+                    <option value="8">8</option>
+                    <option value="9">9</option>
+                    <option value="10">10</option>
+                    <option value="11">11</option>
+                    <option value="12">12</option>
+                </select>
             </div>
         </Modal>
     )
